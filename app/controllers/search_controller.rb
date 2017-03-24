e39c0eb9226a37a92b9fd79daeee83f0a1dd35e0
Rebecca Appleyard <rklappleyard@gmail.com>
2017-03-24 14:03:33 +0000

    * Added query parameter to results page. * Added url to each result on results page. * Added some simple error handling to show no results rather than an error.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,14 +5,18 @@ class SearchController < ApplicationController
   def index; end
 
   def search
-    query_parameter = params[:q]
+    @query_parameter = params[:q]
 
     engine = OpenSearch::OpenSearch.new('http://parliament-search-api.azurewebsites.net/description')
-    response = engine.search(query_parameter)
 
-    @response = JSON.parse(response)
-    @results = @response['Items']
+    begin
+      response = engine.search(@query_parameter)
+      @response = JSON.parse(response)
+      @results = @response['Items']
 
-    render 'results'
+      render 'results'
+    rescue
+      render 'no_results'
+    end
   end
 end
