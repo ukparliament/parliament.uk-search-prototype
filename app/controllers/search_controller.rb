daf0a88777c9fd15b2309df708d44ce6dba6047d
Rebecca Appleyard <rklappleyard@gmail.com>
2017-03-24 11:05:45 +0000

    Removed commented code

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,26 +7,6 @@ class SearchController < ApplicationController
   def search
     query_parameter = params[:q]
 
-    ## Using JSON ##
-
-    # response = Net::HTTP.get(URI(search_endpoint))
-    # @response = JSON.parse(response)
-    #
-    # @results = @response['Items']
-
-
-    ## Using XML ##
-
-    # uri = URI.parse(search_endpoint)
-    # http = Net::HTTP.new(uri.host, uri.port)
-    # request = Net::HTTP::Get.new(uri.request_uri)
-    # request.initialize_http_header({'Accept' => 'application/atom+xml'})
-    #
-    # @response = http.request(request).body
-
-
-    ## Using OpenSearch ##
-
     engine = OpenSearch::OpenSearch.new('http://parliament-search-api.azurewebsites.net/description')
 
     response = engine.search(query_parameter)
