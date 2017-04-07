22bd25fa433886a2142d2475c7216aa7c3864607
Matt Rayner <mattrayner1@gmail.com>
2017-04-07 11:59:11 +0100

    Preparing for Sinatra rewrite

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root 'search#index'
-
-  scope '/search' do
-    get '/', to: 'search#search'
-  end
-end
