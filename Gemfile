e125d135a90ba55892f779f2bf8b8a09ea41752d
Usman Afzal <afzalu@parliament.uk>
2017-04-11 13:26:20 +0100

    [WEBSITE-966] refactoring of views

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 # Use Sinatra for our web server
 gem 'sinatra', '~> 1.4.8'
+gem 'sinatra-contrib'
 
 # Use HAML for our view rendering
 gem 'haml'
@@ -12,12 +13,15 @@ gem 'puma'
 # Use Parliament-Opensearch to handle our Opensearch requests
 gem 'parliament-opensearch'
 
+gem 'pugin', path: '../parliament.uk-pugin-components-rails'
+
+gem 'i18n'
+
 group :development, :test do
   gem 'rubocop'
 end
 
 group :test do
-  gem 'rake'
   gem 'capybara'
   gem 'rspec'
   gem 'simplecov', require: false
--- search.rb
+++ search.rb
@@ -1,9 +1,25 @@
 require 'sinatra'
+require 'sinatra/contrib'
+
+require './multiview'
+
+
+require 'pugin'
+
 require 'parliament'
 require 'parliament/open_search'
 require './helpers/pagination'
 
+require 'i18n'
+require 'i18n/backend/fallbacks'
+
 class Search < Sinatra::Application
+	register Sinatra::MultiView
+
+	#set :views, [Pugin.views_path, 'views']
+	set :view_paths, [ './views/', Pugin.views_path ]
+  set :view_options, { :layout => '/pugin/layouts/pugin-sinatra' }
+
   Parliament::Request::OpenSearchRequest.base_url = ENV['OPENSEARCH_DESCRIPTION_URL']
 
   # TODO: Implement a more robust solution - see http://stackoverflow.com/questions/6221019/is-it-possible-to-to-rewrite-the-base-url-in-sinatra
@@ -17,10 +33,42 @@ class Search < Sinatra::Application
     redirect '/search/'
   end
 
+	helpers do
+		# Construct a link to +url_fragment+, which should be given relative to
+		# the base of this Sinatra app.  The mode should be either
+		# <code>:path_only</code>, which will generate an absolute path within
+		# the current domain (the default), or <code>:full_url</code>, which will
+		# include the site name and port number.  The latter is typically necessary
+		# for links in RSS feeds.  Example usage:
+		#
+		#   link_to "/foo" # Returns "http://example.com/myapp/foo"
+		#
+		#--
+		# Thanks to cypher23 on #mephisto and the folks on #rack for pointing me
+		# in the right direction.
+		def link_to url_fragment, mode=:path_only
+			case mode
+			when :path_only
+				base = request.script_name
+			when :full_url
+				if (request.scheme == 'http' && request.port == 80 ||
+						request.scheme == 'https' && request.port == 443)
+					port = ""
+				else
+					port = ":#{request.port}"
+				end
+				base = "#{request.scheme}://#{request.host}#{port}#{request.script_name}"
+			else
+				raise "Unknown script_url mode #{mode}"
+			end
+			"#{base}#{url_fragment}"
+		end
+	end
+
   get '/' do
     @query_parameter = nil
 
-    haml :'search/index', layout: :'layouts/layout'
+    show 'search/index'
   end
 
   get '/results' do
